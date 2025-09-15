251e5b6a0426e3803a549b411374b8839b142e03
65114340369 <136423433+65114340369@users.noreply.github.com>
2025-09-15 22:07:29 +0700

    Delete Mini Project directory

--- Mini Project/second page.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.buy-button').forEach(button => {
-        button.addEventListener('click', () => {
-            const productName = button.parentElement.querySelector('h3').textContent;
-            alert(`เพิ่ม "${productName}" ลงตะกร้าแล้ว! 🎉`);
-        });
-    });
-});
